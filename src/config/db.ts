e8dca5b107ef322daecd7f1c4382836e4aafaf7f
Pranav Ruparelia <pranavruparelia9@gmail.com>
2025-03-02 18:55:08 -0500

    Remove environment files from Git tracking

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,7 +7,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/my-express-server', {
         });
-
+        
         console.log('MongoDB connected');
     } catch (err) {
         if (err instanceof Error) {
